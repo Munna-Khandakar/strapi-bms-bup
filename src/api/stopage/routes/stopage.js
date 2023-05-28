0cb7a55c1ee2b8f55817c1257a240bd95489ae0f
Munna-Khandakar <munnashisad@gmail.com>
2023-05-28 23:53:03 +0600

    Local Strapi Setup

---
+++ src/api/stopage/routes/stopage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stopage router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::stopage.stopage');
---
+++ src/api/stopage/services/stopage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stopage service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::stopage.stopage');
